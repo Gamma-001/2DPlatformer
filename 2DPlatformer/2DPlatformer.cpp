cef9425deba695510157cec20d617e3066cf1491
Gamma-001 <atikrantnegi@gmail.com>
2024-02-16 23:37:56 +0530

    minor compilation fix

--- 2DPlatformer/2DPlatformer.cpp
+++ 2DPlatformer/2DPlatformer.cpp
@@ -21,7 +21,7 @@ const unsigned int SCR_HEIGHT = 600;
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "2D Platformer", NULL, NULL);
